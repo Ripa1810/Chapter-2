5a85a5c14137a4b23f4646b3ac67e127ef78be6d
Ripa1810 <52244541+Ripa1810@users.noreply.github.com>
2019-07-07 20:27:27 +0600

    Add files via upload

---
+++ findbeerapp/FindBeerActivity.java
@@ -0,0 +1,32 @@
+package com.example.probook.findbeerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+    public void onClickFindBeer(View view){
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.colors);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+        List<String> brandlist = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand : brandlist){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+}
